60d6cc248666cd13ade80a2cecf8f2607f5268c0
ciec20151104691 <2628303302@qq.com>
2017-02-27 11:46:32 +0800

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  0..100
+//
+//  Created by s20151104691 on 17/2/27.
+//  Copyright © 2017年 ltx. All rights reserved.
+//
+
+import Foundation
+
+var arr = String()
+for index in 0...100{
+    arr.append("  \(index)")
+}
+print(arr)
+
+
+
+
+var i:Int = 1
+var j:Int = 0
+
+for i in 0...100
+{
+    j=j+i
+}
+print(j)
